ca4e2f0219cf58a1c1daceb5ee2823a433632174
EricHu <eric.huguangyu@gmail.com>
2025-02-13 22:16:59 +1300

    initail

---
+++ Classes/Circle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1ObjectOriented.Classes
+{
+    public class Circle : Shape
+    {
+        public int radius;
+        public override int GetArea()
+        {
+            return radius;
+        }
+    }
+
+}
